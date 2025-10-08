a7ee75d35be85c8cbaeef233f34432fd745414da
Construction-technect <pankaj.ram@hexafoldtech.com>
2025-10-08 21:39:58 +0530

    `Refactor: Update mobile numbers to string format in userModel and add 404 error handling in index.js`

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(bodyParser.json());
 
 // Mount routes
 app.use("/api", authRoutes);
+
+// Handle 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    status: 404,
+    message: "Route not found",
+  });
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, "0.0.0.0", () => {
